3827b3c802638fd918cb5cb37413e553844f3b29
arpinfidel <richardputra2910@gmail.com>
2026-05-18 13:04:09 +0700

    Add Linux desktop Docker build and launch script
    - Dockerfile.linux: builds native Linux bundle in Docker (Flutter 3.41.6)
    - run-linux.sh: launches via Docker with X11 forwarding (NixOS compatible)
    - Persists app data in ~/.local/share/fluffychat-docker

---
+++ run-linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Launch FluffyChat Linux desktop from Docker container with X11 forwarding
+# Usage: ./run-linux.sh
+
+xhost +local:docker 2>/dev/null
+
+exec docker run --rm \
+  -e DISPLAY="$DISPLAY" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v "$HOME/.local/share/fluffychat-docker:/root/.local/share" \
+  --network host \
+  fluffychat-linux \
+  /app/build/linux/x64/release/bundle/fluffychat
