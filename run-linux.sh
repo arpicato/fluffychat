505db37884d9c3e7ee3579ff6dace571f10cab02
arpinfidel <richardputra2910@gmail.com>
2026-05-18 13:21:39 +0700

    WIP: Linux desktop build and X11 launch script (shebang + xauth fixes)

--- run-linux.sh
+++ run-linux.sh
@@ -6,6 +6,8 @@ xhost +local:docker 2>/dev/null
 
 exec docker run --rm \
   -e DISPLAY="$DISPLAY" \
+  -e XAUTHORITY=/tmp/.xauth \
+  -v "$XAUTHORITY:/tmp/.xauth:ro" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "$HOME/.local/share/fluffychat-docker:/root/.local/share" \
   --network host \
