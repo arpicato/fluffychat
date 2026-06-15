e7ce5d69d71b39ad3344b66c031d1c4d0b545ae0
arpinfidel <richardputra2910@gmail.com>
2026-06-15 17:06:35 +0700

    Fix squash helper merge tag creation

--- scripts/squash_merge_branch.sh
+++ scripts/squash_merge_branch.sh
@@ -11,6 +11,7 @@ FEATURE_BRANCH="$1"
 COMMIT_SUBJECT="$2"
 BASE_BRANCH="${3:-main}"
 TAG_NAME="merged/${FEATURE_BRANCH}"
+FEATURE_TIP="$(git rev-parse "$FEATURE_BRANCH")"
 
 git status --short
 
@@ -22,7 +23,7 @@ fi
 git checkout "$BASE_BRANCH"
 git merge --squash "$FEATURE_BRANCH"
 git commit -m "$COMMIT_SUBJECT" -m "Squash-Merged-From: $FEATURE_BRANCH"
-git tag -a "$TAG_NAME" "$FEATURE_BRANCH" -m "Preserve merged feature branch tip"
+git tag -a "$TAG_NAME" "$FEATURE_TIP" -m "Preserve merged feature branch tip"
 git branch -D "$FEATURE_BRANCH"
 git status --short --branch
 git log --oneline --decorate -6
